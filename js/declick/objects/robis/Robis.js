47860c11ec3546c84e0a19d4e745e203dd55fca9
LiaOkami <pierrick.garcia@hotmail.fr>
2015-12-16 17:28:24 +0100

    Add new object : Robis (remote controlled robot)

---
+++ js/declick/objects/robis/Robis.js
@@ -0,0 +1,44 @@
+define(['jquery', 'TEnvironment', 'TObject', 'TUtils'], function($, TEnvironment, TObject, TUtils) {
+    /**
+     * Defines Robis, inherited from TObject.
+     * Is Robis really real ? :o
+     * @exports Robis
+     */
+    var Robis = function() {
+        TObject.call(this);
+        this.prefix = 'http://';
+        this.suffix = '/declick.py';
+        this.url = 'undefined';
+    };
+
+    Robis.prototype = Object.create(TObject.prototype);
+    Robis.prototype.constructor = Robis;
+    Robis.prototype.className = "Robis";
+    
+    Robis.prototype.command = function(command) {
+        if (this.url !== 'undefined')
+            $.get(this.url, {command: command});
+        else
+            console.debug("Try again"); //Un message mon petit bonhomme ?
+    };
+    
+    Robis.prototype._defineURL = function(url) {
+        this.url = this.prefix + url + this.suffix;
+    };
+    
+    /**
+     * ROBIS WILL WALK. BECAUSE ROBIS IS DA BEST.
+     * (Even if I don't know how yet)
+     */
+    Robis.prototype._moveForward = function() {
+        this.command("fwd");
+    };
+
+    Robis.prototype._stop = function() {
+        this.command("stop");
+    };
+    
+    return Robis;
+});
+
+
