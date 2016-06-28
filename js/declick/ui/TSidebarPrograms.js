f4d52c8a574e529f5dbf849c2b461cb053d9ea9c
Neonunux <regis.leloup@colombbus.org>
2016-06-28 18:25:51 +0200

    programName drag & bug fix onleave click in TSidebarPrograms

--- js/declick/ui/TSidebarPrograms.js
+++ js/declick/ui/TSidebarPrograms.js
@@ -1,61 +1,53 @@
-define(['ui/TComponent', 'TUI', 'TEnvironment', 'TProgram', 'jquery'], function(TComponent, TUI, TEnvironment, TProgram, $) {
+define(['ui/TComponent', 'TUI', 'TEnvironment', 'TProgram', 'jquery'], function (TComponent, TUI, TEnvironment, TProgram, $) {
 
     function TSidebarPrograms(callback) {
 
         var $programs, $list, $buttonDelete;
-        
-
-        TComponent.call(this, "TSidebarPrograms.html", function(component) {
+        var programEdited = false;
+        TComponent.call(this, "TSidebarPrograms.html", function (component) {
             $programs = component;
             $list = component.find("#tsidebar-programs-list");
             $list.addClass("loading");
-            
             var $buttonNewProgram = component.find("#tsidebar-new-program");
             $buttonNewProgram.attr("title", TEnvironment.getMessage('option-new-program'));
-            $buttonNewProgram.click(function(e) {
+            $buttonNewProgram.click(function (e) {
                 TUI.newProgram();
             });
             $buttonDelete = component.find("#tsidebar-delete-program");
             $buttonDelete.attr("title", TEnvironment.getMessage('option-delete'));
-            $buttonDelete.click(function(e) {
+            $buttonDelete.click(function (e) {
                 if (!$(this).is(':disabled')) {
                     TUI.delete();
                 }
             });
-
             if (typeof callback !== 'undefined') {
                 callback.call(this, component);
             }
         });
-
         /**
          * Init Sidebar.
          */
-        this.init = function() {
+        this.init = function () {
             this.update();
         };
-
         /**
          * Loads Programs and Resources.
          */
-        this.load = function() {
+        this.load = function () {
             $list.empty();
             $list.addClass("loading");
         };
-
         /**
          * Update Programs.
          */
-        this.update = function() {
+        this.update = function () {
             $list.removeClass("loading");
             var project = TEnvironment.getProject();
             var programList = project.getProgramsNames();
             var editedPrograms = project.getEditedPrograms();
             var currentProgram = TUI.getCurrentProgram();
             var editedNames = [];
-
             $list.empty();
-
             function addElement(name, id, displayedName, edited, current) {
                 var element = document.createElement("div");
                 element.className = "tsidebar-program";
@@ -65,17 +57,33 @@ define(['ui/TComponent', 'TUI', 'TEnvironment', 'TProgram', 'jquery'], function(
                 if (typeof current !== 'undefined' && current) {
                     element.className += " tsidebar-current";
                 }
-                $(element).click(function(e) {
+
+                $(element).click(function (e) {
+                    console.log("fonction jq click 9");
                     if ($(this).hasClass('tsidebar-renaming'))
                         return false;
                     if (current) {
                         // rename program
                         $(this).addClass('tsidebar-renaming');
+                        programEdited = true;
+                        var self = this;
+                        var checker = function (ev) {
+                            $(self).removeClass('tsidebar-renaming');
+                            renameElement.remove();
+                            programEdited = false;
+                        }
+                        if (programEdited) {
+                            $(window).on('click', checker);
+                        }
+                        else {
+                            $(window).off('click', checker);
+                        }
+
                         var renameElement = document.createElement("input");
                         renameElement.type = "text";
                         renameElement.className = "tsidebar-rename";
                         renameElement.value = name;
-                        $(renameElement).keydown(function(e) {
+                        $(renameElement).keydown(function (e) {
                             if (e.which === 13) {
                                 // Enter was pressed
                                 TUI.renameProgram(name, renameElement.value);
@@ -86,8 +94,13 @@ define(['ui/TComponent', 'TUI', 'TEnvironment', 'TProgram', 'jquery'], function(
                                 $(renameElement).remove();
                             }
                         });
-                        renameElement.onblur = function() {
+
+                        renameElement.onfocus = function () {
+                            element.setAttribute("draggable", "false");
+                        };
+                        renameElement.onblur = function () {
                             TUI.renameProgram(name, renameElement.value);
+                            element.setAttribute("draggable", "true");
                         };
                         $(this).append(renameElement);
                         renameElement.focus();
@@ -97,31 +110,30 @@ define(['ui/TComponent', 'TUI', 'TEnvironment', 'TProgram', 'jquery'], function(
                         e.stopPropagation();
                     }
                     TUI.setEditionEnabled(true);
+                    e.stopPropagation();
                 });
                 var nameElement = document.createElement("div");
                 nameElement.id = "tsidebar-program-" + id;
                 nameElement.appendChild(document.createTextNode(displayedName));
-                element.setAttribute("draggable", "true");
-                element.ondragstart = function (e) {
-                    var programName = $(e.target).find("div").text();
-                    e.dataTransfer.setData("text/plain", "\"" + programName + "\"" );
-                };
-
                 element.appendChild(nameElement);
                 if (edited) {
                     var closeElement = document.createElement("div");
                     closeElement.className = "tsidebar-close";
-                    closeElement.onclick = function(e) {
+                    closeElement.onclick = function (e) {
                         TUI.closeProgram(name);
                         e.stopPropagation();
                     };
                     element.appendChild(closeElement);
                 }
+                element.setAttribute("draggable", "true");
+                element.ondragstart = function (e) {
+                    var el = $(e.target).find("div");
+                    e.dataTransfer.setData("text/plain", "\"" + el.text() + "\"");
+                };
                 $list.append(element);
             }
 
             var currentName = "";
-
             if (typeof currentProgram !== 'undefined') {
                 currentName = currentProgram.getName();
             }
@@ -140,55 +152,45 @@ define(['ui/TComponent', 'TUI', 'TEnvironment', 'TProgram', 'jquery'], function(
                 }
             }
         };
-
-        this.updateInfo = function(program) {
+        this.updateInfo = function (program) {
             var id = "#tsidebar-program-" + program.getId();
             $(id).text(program.getDisplayedName());
         };
-
-        this.showLoading = function(name) {
+        this.showLoading = function (name) {
             var id = "#tsidebar-program-" + TProgram.findId(name);
             var loadElement = document.createElement("div");
             loadElement.className = "tsidebar-loading";
             $(id).append(loadElement);
         };
-
-        this.removeLoading = function(name) {
+        this.removeLoading = function (name) {
             var id = "#tsidebar-program-" + TProgram.findId(name);
             $(id).find(".tsidebar-loading").remove();
         };
-
-        this.showRenaming = function(name) {
+        this.showRenaming = function (name) {
             var id = "#tsidebar-program-" + TProgram.findId(name);
             var loadElement = document.createElement("div");
             loadElement.className = "tsidebar-loading";
             $(id).parent().append(loadElement);
         };
-
-        this.show = function() {
+        this.show = function () {
             $programs.show();
         };
-
-        this.hide = function() {
+        this.hide = function () {
             $programs.hide();
         };
-
-        this.hasCurrent = function() {
+        this.hasCurrent = function () {
             return ($list.find(".tsidebar-current").length > 0);
         };
-
-        this.setEditionEnabled = function(value) {
+        this.setEditionEnabled = function (value) {
             if (value) {
                 $buttonDelete.prop("disabled", false);
             } else {
                 $buttonDelete.prop("disabled", true);
             }
         };
-
     }
 
     TSidebarPrograms.prototype = Object.create(TComponent.prototype);
     TSidebarPrograms.prototype.constructor = TSidebarPrograms;
-
     return TSidebarPrograms;
 });
